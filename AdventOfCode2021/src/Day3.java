9439ae323ede67e0a4db74431e0a5424679cb5ee
Isabella Haladjian <isabellahaladjian@uls.net>
2021-12-14 12:39:41 -0500

    worked on day 3

--- AdventOfCode2021/src/Day3.java
+++ AdventOfCode2021/src/Day3.java
@@ -10,51 +10,53 @@ public class Day3 {
     
     public static void challenge1() {
 
-        try{
+        ArrayList<String> lines = new ArrayList<String>();
 
+        try{
             File input = new File("/Users/isabellahaladjian/Desktop/AdventOfCode-2021/AdventOfCode2021/src/Day3Input.txt");
             Scanner reader = new Scanner(input);
-
-            ArrayList<String> lines = new ArrayList<String>();
-
-            int[] zeroes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            int[] ones = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
             while(reader.hasNext()) {
                 lines.add(reader.next());
             }
+            reader.close(); 
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
 
-            for(int i = 0; i < lines.size(); i++) {
-                for(int b = 0; b < 12; b++) {
-                    if(lines.get(i).indexOf("0", b) == b) {
-                        int counter = zeroes[b];
-                        counter++;
-                        zeroes[b] = counter;
-                    } else if(lines.get(i).indexOf("1", b) == b) {
-                        int counter = ones[b];
-                        counter++;
-                        ones[b] = counter;
-                    }
+        int[] zeroes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] ones = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        for(int i = 0; i < lines.size(); i++) {
+            for(int b = 0; b < 12; b++) {
+                if(lines.get(i).indexOf("0", b) == b) {
+                    int counter = zeroes[b];
+                    counter++;
+                    zeroes[b] = counter;
+                } else if(lines.get(i).indexOf("1", b) == b) {
+                    int counter = ones[b];
+                    counter++;
+                    ones[b] = counter;
                 }
             }
+        }
 
-            int[] gamma = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //more common
-            int[] epsilon = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //less common
+        int[] gamma = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //more common
+        int[] epsilon = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //less common
 
-            for(int i = 0; i < 12; i++) {
-                int numZeroes = zeroes[i];
-                int numOnes = ones[i];
-                if(numZeroes > numOnes) {
-                    gamma[i] = 0;
-                    epsilon[i] = 1;
-                } else if(numOnes > numZeroes) {
-                    gamma[i] = 1;
-                    epsilon[i] = 0;
-                }
-                System.out.println("gamma: " + i + " " + gamma[i]); //gamma: 111011100000 = 32, 64, 128, 512, 1024, 2048 = 3808
-                System.out.println("epsilon: " + i + " " + epsilon[i]); //epsilon: 000100011111 = 1, 2, 4, 8, 16, 256 = 287 
-
+        for(int i = 0; i < 12; i++) {
+            int numZeroes = zeroes[i];
+            int numOnes = ones[i];
+            if(numZeroes > numOnes) {
+                gamma[i] = 0;
+                epsilon[i] = 1;
+            } else if(numOnes > numZeroes) {
+                gamma[i] = 1;
+                epsilon[i] = 0;
             }
+            System.out.println("gamma: " + i + " " + gamma[i]); //gamma: 111011100000 = 32, 64, 128, 512, 1024, 2048 = 3808
+            System.out.println("epsilon: " + i + " " + epsilon[i]); //epsilon: 000100011111 = 1, 2, 4, 8, 16, 256 = 287 
+        }
 
             //TODO: binary to decimal convertor doesn't work, fix this
 
@@ -82,150 +84,123 @@ public class Day3 {
             System.out.println("epsilon: " + epsilonTotal);
             System.out.println("power consumption: " + (gammaTotal * epsilonTotal)); */
 
-            reader.close(); 
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            e.printStackTrace();
-        }
- 
     }
 
     public static void challenge2generator() {
 
-        try{
+        ArrayList<String> lines = new ArrayList<String>();
 
+        try{
             File input = new File("/Users/isabellahaladjian/Desktop/AdventOfCode-2021/AdventOfCode2021/src/Day3Input.txt");
             Scanner reader = new Scanner(input);
-
-            ArrayList<String> lines = new ArrayList<String>();
-
             while(reader.hasNext()) {
                 lines.add(reader.next());
             }
-
-            int zeroesCounter = 0;
-            int onesCounter = 0;
-
-            while(lines.size() > 1) {
-
-                int times = 0;
-
-                for(int i = 0; i < lines.size(); i++) {
-                    if(lines.get(i).indexOf("0", times) == 0) {
-                        zeroesCounter++;
-                    } else if(lines.get(i).indexOf("1", times) == 0) {
-                        onesCounter++;
-                    }
-                }
-    
-                if(zeroesCounter > onesCounter) {
-                    for(int i = 0; i < lines.size(); i++) {
-                        if(lines.get(i).indexOf("1", times) == 0) {
-                            lines.remove(lines.get(i));
-                        }
-                    }
-                } else if(onesCounter > zeroesCounter) {
-                    for(int i = 0; i < lines.size(); i++) {
-                        if(lines.get(i).indexOf("0", times) == 0) {
-                            lines.remove(lines.get(i));
-                        }
-                    }
-                } else if(onesCounter == zeroesCounter) {
-                    for(int i = 0; i < lines.size(); i++) {
-                        if(lines.get(i).indexOf("0", times) == 0) {
-                            lines.remove(lines.get(i));
-                        }
-                    }
-                }
-
-                times++;
-
-                System.out.println("02 Generator Rating: " + lines);
-
-            }
-
-            System.out.println(lines);
-
             reader.close(); 
-
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
         }
- 
+
+        int zeroesCounter = 0;
+        int onesCounter = 0;
+
+        while(lines.size() > 1) {
+            int times = 0;
+            for(int i = 0; i < lines.size(); i++) {
+                if(lines.get(i).indexOf("0", times) == 0) {
+                    zeroesCounter++;
+                } else if(lines.get(i).indexOf("1", times) == 0) {
+                    onesCounter++;
+                }
+            }
+
+            if(zeroesCounter > onesCounter) {
+                for(int i = 0; i < lines.size(); i++) {
+                    if(lines.get(i).indexOf("1", times) == 0) {
+                        lines.remove(lines.get(i));
+                    }
+                }
+            } else if(onesCounter > zeroesCounter) {
+                for(int i = 0; i < lines.size(); i++) {
+                    if(lines.get(i).indexOf("0", times) == 0) {
+                        lines.remove(lines.get(i));
+                    }
+                }
+            } else if(onesCounter == zeroesCounter) {
+                for(int i = 0; i < lines.size(); i++) {
+                    if(lines.get(i).indexOf("0", times) == 0) {
+                        lines.remove(lines.get(i));
+                    }
+                }
+            }
+
+            times++;
+            System.out.println("02 Generator Rating: " + lines);
+        }
     }
 
     public static void challenge2scrubber() {
 
-        try{
+        ArrayList<String> lines = new ArrayList<String>();
 
+        try{
             File input = new File("/Users/isabellahaladjian/Desktop/AdventOfCode-2021/AdventOfCode2021/src/Day3Input.txt");
             Scanner reader = new Scanner(input);
-
-            ArrayList<String> lines = new ArrayList<String>();
-
             while(reader.hasNext()) {
                 lines.add(reader.next());
             }
-
-            int zeroesCounter = 0;
-            int onesCounter = 0;
-
-            while(lines.size() > 1) {
-
-                int times = 0;
-
-                for(int i = 0; i < lines.size(); i++) {
-                    if(lines.get(i).indexOf("0", times) == 0) {
-                        zeroesCounter++;
-                    } else if(lines.get(i).indexOf("1", times) == 0) {
-                        onesCounter++;
-                    }
-                }
-    
-                if(zeroesCounter < onesCounter) {
-                    for(int i = 0; i < lines.size(); i++) {
-                        if(lines.get(i).indexOf("1", times) == 0) {
-                            lines.remove(lines.get(i));
-                        }
-                    }
-                } else if(onesCounter < zeroesCounter) {
-                    for(int i = 0; i < lines.size(); i++) {
-                        if(lines.get(i).indexOf("0", times) == 0) {
-                            lines.remove(lines.get(i));
-                        }
-                    }
-                } else if(onesCounter == zeroesCounter) {
-                    for(int i = 0; i < lines.size(); i++) {
-                        if(lines.get(i).indexOf("0", times) == 0) {
-                            lines.remove(lines.get(i));
-                        }
-                    }
-                }
-
-                times++;
-
-                System.out.println("C02 Scrubber rating: " + lines);
-
-            }
-
-            System.out.println(lines);
-
             reader.close(); 
-
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
         }
- 
+
+        int zeroesCounter = 0;
+        int onesCounter = 0;
+
+        while(lines.size() > 1) {
+
+            int times = 0;
+
+            for(int i = 0; i < lines.size(); i++) {
+                if(lines.get(i).indexOf("0", times) == 0) {
+                    zeroesCounter++;
+                } else if(lines.get(i).indexOf("1", times) == 0) {
+                    onesCounter++;
+                }
+            }
+    
+            if(zeroesCounter < onesCounter) {
+                for(int i = 0; i < lines.size(); i++) {
+                    if(lines.get(i).indexOf("1", times) == 0) {
+                        lines.remove(lines.get(i));
+                    }
+                }
+            } else if(onesCounter < zeroesCounter) {
+                for(int i = 0; i < lines.size(); i++) {
+                    if(lines.get(i).indexOf("0", times) == 0) {
+                        lines.remove(lines.get(i));
+                    }
+                }
+            } else if(onesCounter == zeroesCounter) {
+                for(int i = 0; i < lines.size(); i++) {
+                    if(lines.get(i).indexOf("0", times) == 0) {
+                        lines.remove(lines.get(i));
+                    }
+                }
+            }
+
+            times++;
+            System.out.println("C02 Scrubber rating: " + lines);
+        }
     }
 
     public static void main(String[] args) {
 
-        challenge1();
+        //challenge1();
         challenge2generator();
-        challenge2scrubber();
+        //challenge2scrubber();
 
     }
 
