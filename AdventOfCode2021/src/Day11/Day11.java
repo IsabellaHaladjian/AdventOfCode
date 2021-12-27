1944d5e9e0e886ebbabce68853902acda4d13584
Isabella Haladjian <isabellahaladjian@uls.net>
2021-12-27 14:08:39 -0500

    this code is *pain*

--- AdventOfCode2021/src/Day11/Day11.java
+++ AdventOfCode2021/src/Day11/Day11.java
@@ -1,3 +1,7 @@
+/*
+FIXME: AGHHHHHHHHHHHHHHHHHHH I CAN'T MAKE IT WORK
+*/
+
 package Day11;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,12 +12,13 @@ public class Day11 {
 
     private static ArrayList<String> lines = new ArrayList<String>();
     private static Integer[][] grid = new Integer[10][10];
+    private static int flashCount = 0;
 
     public static void challenge1() {
 
         try {
 
-            File input = new File("/Users/isabellahaladjian/Desktop/AdventOfCode-2021/AdventOfCode2021/src/Day11ExInput.txt");
+            File input = new File("/Users/isabellahaladjian/Desktop/AdventOfCode-2021/AdventOfCode2021/src/Day11/Day11ExInput.txt");
             Scanner reader = new Scanner(input);
     
             while(reader.hasNextLine()) {
@@ -39,18 +44,67 @@ public class Day11 {
         int steps = 1;
 
         while(steps <= 100) {
+
+            boolean[][] hasFlashed = new boolean[10][10];
     
-            for(int i = 0; i < 10; i++) {
-                for(int j = 0; j < 10; j++) {
-                    int energy = grid[i][j];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                int energy = grid[i][j];
+                if(energy >= 9 && !hasFlashed[i][j]) {
+                    //flash(i, j, hasFlashed);
+                    hasFlashed[i][j] = true;
                 }
+                grid[i][j]++;
             }
+        }
 
             steps++;
 
         }
 
     }
+
+/*    public static void flash(int i, int j, boolean[][] hasFlashed) {
+        if(!hasFlashed[i][j]) {
+            grid[i][j] = 0;
+            grid[i-1][j]++;
+            grid[i+1][j]++;
+            grid[i][j+1]++;
+            grid[i][j-1]++;
+            grid[i-1][j-1]++;
+            grid[i+1][j+1]++;
+            grid[i-1][j+1]++;
+            grid[i+1][j-1]++;
+            if(grid[i-1][j] >= 9) {
+                flash(i-1, j);
+            }
+            if(grid[i+1][j] >= 9) {
+                flash(i+1, j);
+            }
+            if(grid[i][j+1]++ >= 9) {
+                flash(i, j+1);
+            }
+            if(grid[i][j-1] >= 9) {
+                flash(i, j-1);
+            }
+            if(grid[i-1][j-1] >= 9) {
+                flash(i-1, j-1);
+            }
+            if(grid[i+1][j+1] >= 9) {
+                flash(i+1, j+1);
+            }
+            if(grid[i-1][j+1] >= 9) {
+                flash(i-1, j+1);
+            }
+            if(grid[i+1][j-1] >= 9) {
+                flash(i+1, j-1);
+            }
+
+            flashCount++;
+        }
+        
+
+    } */
 
     public static void main(String[] args) {
         challenge1();
